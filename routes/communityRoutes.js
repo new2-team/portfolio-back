829c70d84a60d9c2bcb41d95332f1a815b7e109f
yu-jjj <rladbwls0930@gmail.com>
2025-08-10 21:54:57 +0900

    커뮤니티 routes

--- routes/communityRoutes.js
+++ routes/communityRoutes.js
@@ -1,11 +1,28 @@
 // 커뮤니티 관련 라우터 (ES6 import/export)
 
 import express from 'express';
-import { getPosts } from '../controllers/communityController.js';
+import { getPosts, registerPost, removePost, registerReply, removeReply,toggleLike } from '../controllers/communityController.js';
 
-const router = express.Router();
+const communityRoutes = express.Router();
 
 // 게시글 목록 조회 라우트
-router.get('/posts', getPosts);
+router.get('/get-posts', getPosts);
 
-export default router; 
+// 게시글 등록
+router.post('/register-post', registerPost);
+
+// 게시글 삭제
+router.delete('remove-post', removePost);
+
+// 댓글 등록
+router.post('register-reply', registerReply);
+
+// 댓글 삭제
+router.delete('remove-reply', removeReply);
+
+// 좋아요 수 
+router.post('toggle-like', toggleLike);
+
+
+
+export default communityRoutes; 
