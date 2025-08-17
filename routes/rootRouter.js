41178624efae2388a42f27a3514a998f119f3b11
yu-jjj <rladbwls0930@gmail.com>
2025-08-17 23:41:10 +0900

    community 라우트,컨트롤러

--- routes/rootRouter.js
+++ routes/rootRouter.js
@@ -3,6 +3,7 @@ import { index } from "../controllers/index.js";
 import calendarRoutes from "./calendar/calendarRoutes.js";
 import chatRoutes from "./chatting/chatRoutes.js";
 import userRouter from "./user/userRoutes.js";  
+import communityRoutes from "./community/communityRoutes.js";
 
 const rootRouter = express.Router()
 
@@ -10,5 +11,6 @@ rootRouter.get("/", index)
 rootRouter.use("/users", userRouter)  
 rootRouter.use("/calender/api", calendarRoutes)
 rootRouter.use("/chatting/api", chatRoutes)
+rootRouter.use("/community", communityRoutes)
 
 export default rootRouter;
